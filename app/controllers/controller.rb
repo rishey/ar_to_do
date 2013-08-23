1b512b93b3d9c94f4060bd3c2e36b909e1135244
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-23 16:51:30 -0500

    MVP

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -3,20 +3,35 @@ require_relative '../models/list.rb'
 
 # add
 def add(string_array)
-  Task.create!(description: string_array.join(" "))
+  Task.create!(description: string_array.join(" "), 
+               list_id: 1,
+               completed: 0)
+end
+
+def list
+  Task.all
+end
+
+def delete(task_id)
+  task = Task.find_by_id(task_id)
+  task.destroy
+end
+
+def complete(task_id)
+  task = Task.find_by_id(task_id)
+  task.update(completed: 1)
 end
 
 
-
-
-
-# delete
-# complete
-# list
-
 if ARGV.any?
   if ARGV[0] == 'add'
-    p add(ARGV[1..-1])
+    add(ARGV[1..-1])
+  elsif ARGV[0] == 'list'
+    p list
+  elsif ARGV[0] == 'delete'
+    delete(ARGV[1])
+  elsif ARGV[0] == 'complete'
+    complete(ARGV[1])
   end
 
 end
